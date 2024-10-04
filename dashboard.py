98ad835c363523fa698ce94e241c75fc5b347d37
racheldenucci <denuccirachel@gmail.com>
2024-10-04 10:12:12 -0300

    layout fix

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,8 @@ st.set_page_config(
     page_icon=":video_game:", page_title="Video Game Sales Dashboard", layout="wide"
 )
 st.title(":video_game: Video Game Sales")
+st.title("")
+
 
 df = pd.read_csv(r"vgsales.csv", encoding="utf-8")
 
@@ -21,20 +23,21 @@ with col1:
         .reset_index()
     )
 
-    top_5 = top_5.sort_values(by="Global_Sales", ascending=False).head(5)
-
+    top_5 = top_5.sort_values(by="Global_Sales", ascending=False).head(10)
     top_5.index = np.arange(1, len(top_5) + 1)  # index starting from 1
 
     st.subheader(":crown: Top Global Sales")
+    st.write("Sales (millions)")
     st.dataframe(
-        top_5.rename(
-            columns={"Name": "Game", "Global_Sales": "Global Sales (milions)"}
-        )[["Game", "Publisher", "Global Sales (milions)"]],
+        top_5.rename(columns={"Name": "Game", "Global_Sales": "Global Sales"})[
+            ["Game", "Publisher", "Global Sales"]
+        ]
     )
 
+    st.subheader("")
     top_gen = df.groupby(by="Genre").agg({"Global_Sales": "sum"}).reset_index()
 
-    st.subheader("Genres")
+    st.subheader("Sales by Genres")
     fig = px.pie(
         top_gen,
         values="Global_Sales",
@@ -45,26 +48,9 @@ with col1:
 
 
 with col2:
-    # top_pubs = (
-    #     df.groupby(by="Publisher")
-    #     .agg({"Global_Sales": "sum"})
-    #     .reset_index()
-    # )
-
-    # top_pubs = top_pubs.sort_values(by="Global_Sales", ascending=False).head(5)
-
-    # st.subheader(":crown: Top Publishers")
-    # st.dataframe(
-    #     top_pubs.rename(columns={"Global_Sales": "Global Sales (milions)"})[
-    #         ["Publisher", "Global Sales (milions)"]
-    #     ]
-
-    # )
-
     st.subheader("Sales by Publisher")
 
     top_pubs = df.groupby(by="Publisher").agg({"Global_Sales": "sum"}).reset_index()
-
     top_pubs = top_pubs.sort_values(by="Global_Sales", ascending=False).head(5)
 
     fig = px.bar(
@@ -92,26 +78,50 @@ fig = px.line(
     y="Global_Sales",
     color="Platform",
     labels={"Global_Sales": "Global Sales (millions)"},
-    template='simple_white',
-    color_discrete_sequence=px.colors.qualitative.Light24_r
+    template="simple_white",
+    color_discrete_sequence=px.colors.qualitative.Light24_r,
 )
 fig.update_xaxes(title_text="")
 st.plotly_chart(fig)
 
 
-region = st.selectbox('Select a Region', options=['North America', 'Japan', 'Europe', 'Others'])
-if(region == 'North America'):
-    opt = 'NA_Sales'
-elif(region == 'Japan'):
-    opt = 'JP_Sales'
-elif(region=='Europe'):
-    opt='EU_Sales'
-elif(region=='Others'):
-    opt='Other_Sales'
+col1, col2 = st.columns(2)
 
-top_pubs_filter = df.groupby(by='Publisher').agg({opt: 'sum'}).reset_index()
-top_pubs_filter = top_pubs_filter.sort_values(by=opt, ascending=False).head(5)
+with col1:
+    region = st.selectbox(
+        "Select a Region", options=["North America", "Japan", "Europe", "Others"]
+    )
+    if region == "North America":
+        opt = "NA_Sales"
+    elif region == "Japan":
+        opt = "JP_Sales"
+    elif region == "Europe":
+        opt = "EU_Sales"
+    elif region == "Others":
+        opt = "Other_Sales"
 
-fig = px.bar(top_pubs_filter, x='Publisher', y=opt, template='simple_white', labels={opt:region, 'Publisher':' '})
-st.plotly_chart(fig)
+    st.subheader(f"Best Selling in {region}")
+    st.write("Sales (millions of copies)")
 
+    top_5_filter = (
+        df.groupby(by="Name").agg({opt: "sum", "Publisher": "first"}).reset_index()
+    )
+    top_5_filter = top_5_filter.sort_values(by=opt, ascending=False).head(5)
+    top_5_filter.index = np.arange(1, len(top_5_filter) + 1)
+
+    st.dataframe(top_5_filter.rename(columns={"Name": "Game", opt: f"{region} Sales"}))
+
+with col2:
+    st.header("")
+    top_pubs_filter = df.groupby(by="Publisher").agg({opt: "sum"}).reset_index()
+    top_pubs_filter = top_pubs_filter.sort_values(by=opt, ascending=False).head(5)
+
+    fig = px.bar(
+        top_pubs_filter,
+        x="Publisher",
+        y=opt,
+        title="Publisher Sales",
+        template="plotly_dark",
+        labels={opt: f"{region} Sales", "Publisher": " "},
+    )
+    st.plotly_chart(fig)
