96b926acab0625578e83efc954ac23109cade041
racheldenucci <denuccirachel@gmail.com>
2024-10-22 18:11:24 -0300

    update: forecasting with prophet

--- pages/Nintendo.py
+++ pages/Nintendo.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+import plotly.graph_objects as go
+from prophet import Prophet
 
 st.set_page_config(page_icon=":video_game:", layout="wide")
 
@@ -65,8 +67,8 @@ st.plotly_chart(fig)
 # TOP 5 GAMES
 c1, c2 = st.columns(2)
 with c1:
-    st.subheader(f'Best Sellers')
-    st.write(f'({start} - {end})')
+    st.subheader(f"Best Sellers")
+    st.write(f"({start} - {end})")
     top_5 = (
         filt_df.groupby("Name")
         .agg({"Global_Sales": "sum", "Genre": "first", "Year": "first"})
@@ -95,30 +97,76 @@ if evt_data:
         game = evt_data["selection"]["points"][0]["hovertext"]
 
         with c2:
-            st.subheader(f'{game} Sales')
-            st.write(f'({start} - {end})')
+            st.subheader(f"{game} Sales")
+            st.write(f"({start} - {end})")
             game_df = filt_df[filt_df["Name"] == game].iloc[0]
-            region_sales = pd.DataFrame({
-                'Region':['North America', 'Europe', 'Japan', 'Others'],
-                'Sales': [game_df['NA_Sales'], game_df['EU_Sales'], game_df['JP_Sales'], game_df['Other_Sales']]
-            })
+            region_sales = pd.DataFrame(
+                {
+                    "Region": ["North America", "Europe", "Japan", "Others"],
+                    "Sales": [
+                        game_df["NA_Sales"],
+                        game_df["EU_Sales"],
+                        game_df["JP_Sales"],
+                        game_df["Other_Sales"],
+                    ],
+                }
+            )
 
             fig = px.bar(
                 region_sales,
-                x='Region',
-                y='Sales',
-                labels={
-                    'Region':''
-                },
-                template='presentation',
-                color_discrete_sequence=['violet'],
-                text=region_sales['Sales'].apply(lambda y: f'{y:.1f} M')
+                x="Region",
+                y="Sales",
+                labels={"Region": ""},
+                template="presentation",
+                color_discrete_sequence=["violet"],
+                text=region_sales["Sales"].apply(lambda y: f"{y:.1f} M"),
             )
             st.plotly_chart(fig)
     except KeyError:
         st.error("Sorry, something went wrong")
     except IndexError:
         st.write("Select a game to see details")
-    
+
 else:
     st.write("please select a game")
+
+
+# FORECASTING WITH PROPHET
+st.subheader("Forecasting")
+df["Year"] = pd.to_datetime(df["Year"], format="%Y")
+ts = df.groupby("Year").agg({"Global_Sales": "sum"}).reset_index()
+
+ts.columns = ["ds", "y"]
+
+# changepoint_prior_scale is way too high and VERY incorrect
+model = Prophet(yearly_seasonality=False, changepoint_prior_scale=0.8).fit(ts)
+future = model.make_future_dataframe(10, freq="YS")
+forecast = model.predict(future)
+
+# --- would show significant changes if there were any
+# from prophet.plot import add_changepoints_to_plot
+# fig = model.plot(forecast)
+# add_changepoints_to_plot(fig.gca(), model, forecast)
+# st.write(fig)
+
+# st.write(model.plot_components(forecast))
+# st.line_chart(forecast, y='yhat', x='ds')
+
+only_future_forecast = forecast[forecast['ds']>='2016-01-01']
+
+fig = px.line(
+    only_future_forecast, x="ds", y="yhat", labels={"ds": "Year", "yhat": "Forecasted Sales"} 
+)
+fig.data[0].update(line=dict(dash='dash'))
+
+fig.add_scatter(x=ts['ds'], y=ts['y'], mode='lines', name='Actual Sales', line=dict(color='orchid'))
+fig.add_vline(x='2016', line_dash='dash', line_color='gray', line_width=1)
+fig.add_annotation(
+    x='2016',
+    y = max(ts['y']),
+    text='Sales Forecasting', 
+    showarrow=False,
+    bgcolor='#0E1117'
+
+)
+st.plotly_chart(fig)
